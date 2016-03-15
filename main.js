acffaf15810c42be5c125122cdf2e1d9f09b32cd
Brandon Stansbury <brandon.stansbury@infospace.com>
2016-03-15 12:00:29 -0700

    implement basic subscription stuff

--- main.js
+++ main.js
@@ -1,33 +1,72 @@
-function setUIState() {
-  if (Notification.permission === 'granted') {
-    $('#theButton').text('Unsubscribe');
-    $('#output').text('Click unsubscribe to do something');
-  } else if (Notification.permission === 'denied') {
-    $('#theButton').prop('disabled', true);
-    $('#output').text('You have rejected notifications from this domain');
-  } else {
-    $('#theButton').text('Subscribe');
-    $('#output').text('Click subscribe to do get push notifications from me');
-  }
+function writeMsg(msg) {
+  $('#output').text(msg);
 }
 
 $(function () {
   if (!Notification) {
-    $('#output').text('Push notifications not supported!');
+    writeMsg('Push notifications not supported!');
     return;
   }
 
+  if (!navigator.serviceWorker) {
+    writeMsg('Service worker not supported!');
+    return;
+  }
+
+  $('#subscribe').click(function () {
+    $('#subscribe').prop('disabled', true);
+    navigator.serviceWorker.ready.then(function (reg) {
+      reg.pushManager.subscribe({userVisibleOnly: true}).then(function (sub) {
+        writeMsg('Subscribed successfully');
+        $('#subscription').text(sub.endpoint);
+        $('#unsubscribe').prop('disabled', false);
+      }).catch(function (e) {
+        writeMsg('SUB ERROR: ' + e);
+        $('#subscribe').prop('disabled', false);
+      });
+    })
+  });
+
+  $('#unsubscribe').click(function () {
+    $('#unsubscribe').prop('disabled', true);
+    navigator.serviceWorker.ready.then(function (reg) {
+      reg.pushManager.getSubscription().then(function (sub) {
+        sub.unsubscribe().then(function () {
+          $('#subscribe').prop('disabled', false);
+          writeMsg('Successfully unsubscribed');
+          $('#subscription').text('');
+        }).catch(function (e) {
+          writeMsg('UNSUB ERROR: ' + e);
+          $('#unsubscribe').prop('disabled', false);
+        })
+      });
+    });
+  });
+
   Notification.requestPermission();
 
-  setUIState();
-
-  $('#theButton').click(function () {
-    if ($('#theButton').text() === 'Subscribe') {
-      Notification.requestPermission()
-        .then(function (permission) {
-          alert(permission);
-          setUIState();
-        });
+  navigator.serviceWorker.register('sw.js').then(function (reg) {
+    if (reg.installing) {
+      writeMsg('Installing service worker...');
+    } else if (reg.waiting) {
+      writeMsg('Service worker waiting...');
+    } else if (reg.active) {
+      writeMsg('Service worker ready!');
     }
+
+    navigator.serviceWorker.ready.then(function (reg) {
+      reg.pushManager.getSubscription().then(function (sub) {
+        if (sub) {
+          writeMsg('Found current subscription');
+          $('#subscription').text(sub.endpoint);
+          $('#subscribe').prop('disabled', true);
+          $('#unsubscribe').prop('disabled', false);
+        } else {
+          writeMsg('Click subscribe to create a subscription');
+          $('#subscribe').prop('disabled', false);
+          $('#unsubscribe').prop('disabled', true);
+        }
+      });
+    });
   });
 })
